9c0f8ca6700e0aa83f6dcddf7eb79c606b892d3a
guoj <dev.guoj@gmail.com>
2016-06-09 19:35:13 +0800

    test cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+template <typename T>
+int compare(const T &v1, const T &v2) {
+    if (v1 < v2) {
+        return -1;
+    } else if (v1 > v2) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << compare(4, 5) << endl;
+    cout << compare(4.5, 3.2) << endl;
     return 0;
 }
