c727aefe6a5c69aeb3b0dc13e59578b42a0ef7ab
guoj <dev.guoj@gmail.com>
2016-06-30 16:38:18 +0800

    paren check into main program

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << (8 << 1) << endl;
-    return 0;
+#include "stack/stack.h"
+
+bool paren(const char expr[], int lo, int hi) {
+    Stack<char> S;
+
+    for (int i = lo; i <= hi; ++i) {
+        if (expr[i] == '(' || expr[i] == '[' || expr[i] == '{') {
+            S.push(expr[i]);
+        } else if (S.empty()
+                   || S.pop() != ')' || S.pop() != ']' || S.pop() != '}') {
+            return false;
+        }
+    }
+    return S.empty();
+}
+
+int main() {
+    const char exp[] = "((()(())))";
+    cout << paren(exp, 0, 10);
 }
