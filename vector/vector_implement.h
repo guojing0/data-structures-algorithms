c9c11238909e856b6a32e8f35eb02b95bc3a94b5
guoj <dev.guoj@gmail.com>
2016-07-02 00:29:23 +0800

    vector header for implement

---
+++ vector/vector_implement.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "vector_assignment.h"
+#include "vector_bracket.h"
+
+#include "copy_constructor.h"
+
+#include "vector_expand.h"
+#include "vector_shrink.h"
+
+#include "vector_insert.h"
+#include "vector_remove.h"
+#include "vector_removeinternal.h"
+
+#include "vector_disordered.h"
+#include "vector_find.h"
+#include "vector_search.h"
+#include "vector_fib_search_a.h"
+#include "vector_bin_search_c.h"
+
+#include "vector_traverse.h"
+
+#include "vector_sort.h"
+#include "vector_unsort.h"
+#include "vector_bubble.h"
+#include "vector_bubblesort.h"
+//#include "selection sort"
+#include "vector_merge.h"
+#include "vector_mergesort.h"
+#include "vector_partition.h"
+//#include "vector_quickSort.h"
+//#include "vector_heapSort.h"
+
+#include "vector_uniquify.h"
+#include "vector_deduplicate.h"
