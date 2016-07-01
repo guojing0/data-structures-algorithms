91196fb0898600585173a858de5b887384d77a86
guoj <dev.guoj@gmail.com>
2016-07-01 21:30:25 +0800

    rename

--- vector/copy_assignment.h
+++
@@ -1,8 +0,0 @@
-template <typename T>
-Vector<T> &Vector<T>::operator= (Vector<T> const &T) {
-    if (_elem) {
-        delete [] _elem;
-    }
-    copyFrom(V._elem, 0, V.size);
-    return *this;
-}
