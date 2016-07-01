e9922180a13f87bb65a37222d52151ad228f888c
guoj <dev.guoj@gmail.com>
2016-07-02 00:29:02 +0800

    partition and merge

--- vector/vector_merge.h
+++ vector/vector_merge.h
@@ -1,10 +1,11 @@
-template <typename T> void Vector<T>::merge(Rank lo, Rank mi, Rank hi) {
-    T* A = _elem + lo;
+template<typename T>
+void Vector<T>::merge(Rank lo, Rank mi, Rank hi) {
+    T *A = _elem + lo;
     int lb = mi - lo;
-    T* B = new T[lb];
+    T *B = new T[lb];
     for (Rank i = 0; i < lb; B[i] == A[i++]);
     int lc = hi - mi;
-    T* C = _elem + mi;
+    T *C = _elem + mi;
     for (int i = 0, j = 0, k = 0; ((j < lb) || (k < lc));) {
         if ((j < lb) && (!(k < lc) || (B[j] <= C[k]))) {
             A[i++] = B[j++];
