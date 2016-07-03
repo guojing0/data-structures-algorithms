c13fb8683d3916813576ca89e6c64190ba1ae7d2
guoj <dev.guoj@gmail.com>
2016-07-03 09:07:34 +0800

    debug

--- vector/vector_disordered.h
+++ vector/vector_disordered.h
@@ -1,9 +1,9 @@
 template <typename T> int Vector<T>::disordered() const {
     int n = 0;
 
-    for (int i = 1; i < _size; i++) {
+    for (int i = 1; i < _size; ++i) {
         if (_elem[i - 1] > _elem[i]) {
-            n = n + 1;
+            ++n;
         }
     }
     return n;
--- vector/vector_merge.h
+++ vector/vector_merge.h
@@ -3,7 +3,7 @@ void Vector<T>::merge(Rank lo, Rank mi, Rank hi) {
     T *A = _elem + lo;
     int lb = mi - lo;
     T *B = new T[lb];
-    for (Rank i = 0; i < lb; B[i] == A[i++]);
+    for (Rank i = 0; i < lb; B[i] = A[i++]);
     int lc = hi - mi;
     T *C = _elem + mi;
     for (int i = 0, j = 0, k = 0; ((j < lb) || (k < lc));) {
