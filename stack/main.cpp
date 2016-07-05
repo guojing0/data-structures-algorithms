7ad81d8ec25beab93f0c1a0773ee50a16ef59e85
guoj <dev.guoj@gmail.com>
2016-07-05 15:48:26 +0800

    stack paren match

--- stack/main.cpp
+++ stack/main.cpp
@@ -3,34 +3,40 @@
 
 using namespace std;
 
-void convertIter(Stack<char>& S, __int64_t n, int base) {
-    static char digit[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                           'A', 'B', 'C', 'D', 'E', 'F'};
-    while (n > 0) {
-        int remainder = (int) (n % base);
-        S.push(digit[remainder]);
-        n /= base;
-    }
-}
+bool paren(const char expr[], int lo, int hi)
+{
+    Stack<char> S;
 
-void convertRec(Stack<char>& S, __int64_t n, int base) {
-    static char digit[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                           'A', 'B', 'C', 'D', 'E', 'F'};
-    if (n > 0) {
-        convertRec(S, n / base, base);
-        S.push(digit[n % base]);
+    for (int i = lo; i <= hi; ++i) {
+        switch (expr[i]) {
+            case '(': case '[': case '{':
+                S.push(expr[i]);
+                break;
+            case ')':
+                if ((S.empty()) || (S.pop() != '(')) {
+                    return false;
+                }
+                break;
+            case ']':
+                if ((S.empty()) || (S.pop() != '[')) {
+                    return false;
+                }
+                break;
+            case '}':
+                if ((S.empty()) || (S.pop() != '{')) {
+                    return false;
+                }
+                break;
+            default:
+                break;
+        }
     }
+    return S.empty();
 }
 
 int main()
 {
-    Stack<char> stack1;
-    convertIter(stack1, 256, 2);
-
-    while (!stack1.empty()) {
-        cout << stack1.pop() << "";
-    }
-    cout << endl;
-
+    const char foo[] = "())";
+    cout << paren(foo, 0, (int) (strlen(foo) - 1)) << endl;
     return 0;
 }
--- stack/stack_paren.cpp
+++ stack/stack_paren.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
+#include "stack.h"
+
 using namespace std;
 
-#include "stack/stack.h"
-
-bool paren(const char expr[], int lo, int hi) {
+bool paren(const char expr[], int lo, int hi)
+{
     Stack<char> S;
 
     for (int i = lo; i <= hi; ++i) {
-        if (expr[i] == '(' || expr[i] == '[' || expr[i] == '{') {
-            S.push(expr[i]);
-        } else if (S.empty()
-                   || S.pop() != ')' || S.pop() != ']' || S.pop() != '}') {
-            return false;
+        switch (expr[i]) {
+            case '(': case '[': case '{':
+                S.push(expr[i]);
+                break;
+            case ')':
+                if ((S.empty()) || (S.pop() != '(')) {
+                    return false;
+                }
+                break;
+            case ']':
+                if ((S.empty()) || (S.pop() != '[')) {
+                    return false;
+                }
+                break;
+            case '}':
+                if ((S.empty()) || (S.pop() != '{')) {
+                    return false;
+                }
+                break;
+            default:
+                break;
         }
     }
     return S.empty();
 }
 
-int main() {
-    const char exp[] = "((()(())))";
-    cout << paren(exp, 0, 10);
+int main()
+{
+    const char foo[] = "(()()";
+    cout << paren(foo, 0, (int) (strlen(foo) - 1)) << endl;
+    return 0;
 }
